1c1e8cfb7faeac58a99fc3d478c235d36cd90218
srinukindia <srinukindia@gmail.com>
2025-02-14 18:49:42 -0600

    Fetch Rewards Coding Exercise - Analytics Engineer

---
+++ 02_FETCH_ANSWERS.sql
@@ -0,0 +1,36 @@
+------------------------------------------------------------------------------------------
+/*#02A]What are the top 5 brands by receipts scanned for most recent month?*/
+------------------------------------------------------------------------------------------
+--snowSQL
+--Assuming timestamps given are epoch timestamps
+SELECT
+	I.BRANDCODE,
+	COUNT(*)
+FROM RECEIPTS_FACT R
+JOIN ITEMS_DIM I
+ON I.ITEMID_DKEY= R.ITEMID_DKEY
+WHERE TO_DATE(TO_TIMESTAMP(R.DATESCANNE))>= DATE_TRUNC('MONTH',CURRENT_DATE)
+GROUP BY I.BRANDCODE
+HAVING COUNT(*) >1
+ORDER BY COUNT(*) DESC
+LIMIT 5 ;
+
+------------------------------------------------------------------------------------------
+/*#02F]Which brand has the most transactions among users who were created within the past 6 months?*/
+------------------------------------------------------------------------------------------
+--snowSQL
+--Assuming timestamps given are epoch timestamps
+--Assuming every entry in Fact table as 1 transaction independent of receipts ID (as its only part of PK fields)  is repetitive or not.
+SELECT
+	I.BRANDCODE,
+	COUNT(*)
+FROM RECEIPTS_FACT R
+JOIN ITEMS_DIM I
+ON I.ITEMID_DKEY= R.ITEMID_DKEY
+JOIN USERS_DIM U
+ON U.USERID_DKEY= R.USERID_DKEY
+WHERE TO_DATE(TO_TIMESTAMP(U.CREATEDDATE))>= DATEADD('MONTH',-6,DATE_TRUNC('MONTH',CURRENT_DATE))
+GROUP BY I.BRANDCODE
+HAVING COUNT(*) >1
+ORDER BY COUNT(*) DESC
+LIMIT 1;
---
+++ 03_FETCH_DATAQUALITY.sql
@@ -0,0 +1,45 @@
+------------------------------------------------------------------------------------------
+/*#03] DATA QUALIFY Examples from given data.*/
+------------------------------------------------------------------------------------------
+--If SQL returns more than 1 row, its a data quality issue
+
+------------------------------------------------------------------------------------------
+--#1/NULL examples
+--snowSQL
+SELECT
+	'NULLS' As DQ_CASE_DESCRIPTION,
+	'PURCHASEDATE' As FIELD,
+	PURCHASEDATE As VALUE
+FROM RECEIPTS
+WHERE PURCHASEDATE IS NULL;
+
+SELECT
+	'NULLS' As DQ_CASE_DESCRIPTION
+	DATESCANNED
+FROM RECEIPTS
+WHERE DATESCANNED IS NULL;
+
+------------------------------------------------------------------------------------------
+--#2/DATA ISSUES1
+--Assuming timestamps given are epoch timestamps
+--snowSQL
+SELECT 
+	'DATA_ISSUES/OUT_OF_RANGE' As DQ_CASE_DESCRIPTION,
+	'LASTLOGIN' As FIELD,
+	LASTLOGIN As VALUE
+FROM USERS
+WHERE TO_DATE(TO_TIMESTAMP(LASTLOGIN))>= CURRENT_DATE+1 ; --to offset issues with timezones
+
+
+------------------------------------------------------------------------------------------
+--#3/DATA ISSUES2
+--Assuming Name field should not special characters
+--snowSQL
+SELECT 
+	'DATA_ISSUES/SPECIAL_CHARACTERS' As DQ_CASE_DESCRIPTION,
+	'NAME' As FIELD,
+	NAME As VALUE
+FROM BRANDS
+WHERE NOT REGEXP_LIKE(NAME,'^[a-zA-Z0-9 ]+$');
+
+
